f869aa9bd13e84255d63414dace00d0b300e209e
Kemps Medeiros <kemps.medeiros@kabum.com.br>
2024-03-13 18:26:48 -0300

    feat: cpf validator

--- src/clean_code_refactoring/cpf_validator.ts
+++ src/clean_code_refactoring/cpf_validator.ts
@@ -1,56 +1,25 @@
-const MINIMUM_NUMBER_OF_DIGITS = 11;
-const MAXIMUM_NUMBER_OF_DIGITS = 14;
+const NUMBER_OF_DIGITS = 11;
+const FACTOR_TO_CALCULATE_DIGIT_ONE = 10;
+const FACTOR_TO_CALCULATE_DIGIT_TWO = 11;
 
 
 export function validateCpf(cpf: string) {
 
     if (!cpf) return false;
 
-    if(cpf.length < MINIMUM_NUMBER_OF_DIGITS || cpf.length > MAXIMUM_NUMBER_OF_DIGITS) return false; 
-
     const cpfString = getStringOnly(cpf); 
+    if(cpfString.length != NUMBER_OF_DIGITS) return false; 
 
     if(isAllDigitsAreTheSame(cpfString)) return false;
+
+    const firstVerificationDigit = getFirstVerificationDigit(cpfString);
+    const secondVerificationDigit = getSecondVerificationDigit(firstVerificationDigit, cpfString);
+
+    const resultExpected = `${firstVerificationDigit}${secondVerificationDigit}`;
+
+    const digitsToValidate = cpfString.slice(cpfString.length -2);
     
-    try{  
-        let     d1, d2;  
-        let     dg1, dg2, rest;  
-        let     digito;  
-            let     nDigResult;  
-        d1 = d2 = 0;  
-        dg1 = dg2 = rest = 0;  
-            
-        for (let nCount = 1; nCount < cpfString.length -1; nCount++) {  
-            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-            // 	return false;
-            // } else {
-
-                digito = parseInt(cpfString.substring(nCount -1, nCount));  							
-                d1 = d1 + ( 11 - nCount ) * digito;  
-
-                d2 = d2 + ( 12 - nCount ) * digito;  
-            // }
-        };  
-            
-        rest = (d1 % 11);  
-
-        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-        d2 += 2 * dg1;  
-        rest = (d2 % 11);  
-        if (rest < 2)  
-            dg2 = 0;  
-        else  
-            dg2 = 11 - rest;  
-
-            let nDigVerific = cpfString.substring(cpfString.length-2, cpfString.length);  
-        nDigResult = "" + dg1 + "" + dg2;  
-        return nDigVerific == nDigResult;
-    }catch (e){  
-        console.error("Erro !"+e);  
-
-        return false;  
-    }  
-    
+    return digitsToValidate === resultExpected;
 
 }
 
@@ -66,3 +35,30 @@ function isAllDigitsAreTheSame(cpfString: string) {
     return cpfString.split("").every(digit => digit === cpfString[0]);
 }
 
+function getFirstVerificationDigit(cpf: string) {
+    let firstNineCharacter = cpf.slice(0, -2);
+    const firstDigit = calculateVerificatorDigitByFactor(FACTOR_TO_CALCULATE_DIGIT_ONE, firstNineCharacter);
+    return firstDigit;
+}
+
+function getSecondVerificationDigit(firstDigitVerificator: string, cpf: string) {
+    let firstNineCharacter = cpf.slice(0, -2);
+    let cpfStringMoreOneDigit = firstNineCharacter + firstDigitVerificator;
+    const secondDigit = calculateVerificatorDigitByFactor(FACTOR_TO_CALCULATE_DIGIT_TWO, cpfStringMoreOneDigit);
+    return secondDigit;
+}
+
+function calculateVerificatorDigitByFactor(factor: number, cpfStringBase: string) {
+    let counter = factor;
+    let sum = 0;
+    for (const number of cpfStringBase) {
+        sum = sum + (Number(number) * counter)
+        counter--
+    }
+
+    let remainderOfDivision = sum%11;
+
+    const result = remainderOfDivision < 2 ? 0 : (11 - remainderOfDivision);
+
+    return result.toString();
+}
